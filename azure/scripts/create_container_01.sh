0c9bd07db811a01c2ac2dbd62b55ec56094e336f
claudou <claudou74@gmail.com>
2019-06-03 12:34:14 +0100

    Update create_container_01.sh

--- azure/scripts/create_container_01.sh
+++ azure/scripts/create_container_01.sh
@@ -11,6 +11,6 @@ az container create \
 	--subscription Pay-As-You-Go \
 	--restart-policy OnFailure \
 	--location westeurope \
-	--environment-variables DD_API_KEY=57b752e8a4a68a1eafbd340a03bdbbb3 DD_APM_ENABLED=true DD_HOSTNAME=cl-2 DD_TAGS=CL-TAG DD_APM_NON_LOCAL_TRAFFIC=true DD_LOGS_ENABLED=true \
-	--azure-file-volume-share-name containershare --azure-file-volume-account-name claudou --azure-file-volume-account-key SJH2STMwL7jrHGkQcWzVPdOVRp7Nk+qzodrICSXVSec5flUzzcTOy0hbIvasiQDESJwh+8GV9AHVgj01jErogA== --azure-file-volume-mount-path /conf.d
+	--environment-variables DD_API_KEY=#### DD_APM_ENABLED=true DD_HOSTNAME=cl-2 DD_TAGS=CL-TAG DD_APM_NON_LOCAL_TRAFFIC=true DD_LOGS_ENABLED=true \
+	--azure-file-volume-share-name containershare --azure-file-volume-account-name claudou --azure-file-volume-account-key #### --azure-file-volume-mount-path /conf.d
     
